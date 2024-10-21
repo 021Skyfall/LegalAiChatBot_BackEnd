8829450908f5ae650ea9f6428da7c50632afb31f
jerry <021skyfall@gmail.com>
2024-10-21 19:30:12 +0900

    project bases

---
+++ src/main/java/codeG/LCB/LcbApplication.java
@@ -0,0 +1,13 @@
+package codeG.LCB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeG/LCB/LcbApplicationTests.java
@@ -0,0 +1,13 @@
+package codeG.LCB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
